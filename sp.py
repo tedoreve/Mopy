71f7b0107ef5930497e251cdfc89479549f8ef98
tedoreve <wlzzs666@gmail.com>
2017-04-25 11:10:57 +0800

    auto commit

--- sp.py
+++ sp.py
@@ -7,7 +7,7 @@ Created on Sun Feb 19 18:49:51 2017
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+#import time
 
 #==============================================================================
         
@@ -15,7 +15,6 @@ def download(url,post,t):
 
 #    driver = webdriver.PhantomJS()
     driver = webdriver.Firefox()
-#    driver.set_page_load_timeout(t)
     driver.get(url)    
 #    print(driver.page_source)
 
@@ -23,25 +22,6 @@ def download(url,post,t):
     box.send_keys(Keys.TAB)
     box.send_keys(post[0])
     box.send_keys(Keys.ENTER)
-#    print()
-#    print('第'+str(index+1)+'个视频全屏成功')
-#    #防止命令太近反应不过来
-#    time.sleep(timeofbuffer)
-#    #播放
-#    driver.find_element_by_xpath(element).click()
-#    print()
-#    print('第'+str(index+1)+'个视频播放成功')
-
-    #随时检测是否在播放，一旦关闭播放器就重新开一个
-#    try:
-#        for j in range(int(int(timelist[index].strip())/timeofnext)):
-#            driver.current_url
-#            #视频持续时间
-#            time.sleep(timeofnext)
-    
-
-
-
 
 #==============================================================================
 if __name__=='__main__':
